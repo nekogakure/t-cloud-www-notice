fe91629deeb65bcf3a84baef17eaabfc085073df
ねこがくれ <osjin83@gmail.com>
2025-01-21 17:17:31 +0900

    jsonにリストアップするように変更

--- convert.py
+++ convert.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import markdown
+import json
 
 if len(sys.argv) < 2:
-    print("Usage: python main.py <markdown_file>\n作業するディレクトリはnoticeディレクトリである必要があります。")
+    print("Usage: python convert.py <markdown_file>")
     sys.exit(1)
 
 file_path = sys.argv[1]
@@ -26,3 +27,21 @@ with open(output_file_path, 'w', encoding='utf-8') as f:
     f.write(html_content)
 
 print(f"Converted HTML file saved to: {output_file_path}")
+
+# index.jsonにリストアップ
+index_file_path = os.path.join(output_dir, 'index.json')
+if os.path.exists(index_file_path):
+    with open(index_file_path, 'r', encoding='utf-8') as f:
+        index_data = json.load(f)
+else:
+    index_data = []
+
+index_data.append({
+    'markdown_file': file_path,
+    'html_file': output_file_path
+})
+
+with open(index_file_path, 'w', encoding='utf-8') as f:
+    json.dump(index_data, f, ensure_ascii=False, indent=4)
+
+print(f"Updated index.json with: {output_file_path}")
