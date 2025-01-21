9dc709c6f94b92bc324abd3e4b2f75dc0821e4a7
ねこがくれ <156896971+nekogakure@users.noreply.github.com>
2025-01-21 17:54:19 +0900

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import markdown
 import json
 
 if len(sys.argv) < 2:
@@ -10,23 +9,9 @@ if len(sys.argv) < 2:
 file_path = sys.argv[1]
 absolute_path = os.path.abspath(file_path)
 
-with open(absolute_path, 'r', encoding='utf-8') as f:
-    target_file = f.read()
-
-md = markdown.Markdown(extensions=['tables'])
-html_content = md.convert(target_file)
-
-# 出力ディレクトリとファイル名の設定
+# 出力ディレクトリの設定
 output_dir = 'html'
 os.makedirs(output_dir, exist_ok=True)
-output_file_name = os.path.splitext(os.path.basename(file_path))[0] + '.html'
-output_file_path = os.path.join(output_dir, output_file_name)
-
-# HTMLファイルに書き込み
-with open(output_file_path, 'w', encoding='utf-8') as f:
-    f.write(html_content)
-
-print(f"Converted HTML file saved to: {output_file_path}")
 
 # index.jsonにリストアップ
 index_file_path = os.path.join(output_dir, 'index.json')
@@ -38,10 +23,9 @@ else:
 
 index_data.append({
     'markdown_file': file_path,
-    'html_file': output_file_path
 })
 
 with open(index_file_path, 'w', encoding='utf-8') as f:
     json.dump(index_data, f, ensure_ascii=False, indent=4)
 
-print(f"Updated index.json with: {output_file_path}")
+print(f"Updated index.json with markdown file: {file_path}")
